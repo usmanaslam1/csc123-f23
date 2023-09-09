52a2647136ff58359dc6083836b04039c7c933b3
usman <usman@usmans-mbp>
2023-09-09 13:10:30 -0700

    first commit
    Signed-off-by: usman <usman@usmans-mbp>

---
+++ CSC123-Resources/Week2/OpPrec.java
@@ -0,0 +1,18 @@
+
+public class OpPrec {
+	
+	public static void main(String[] args) {
+		
+		int x=10/5+3*2-3;
+		
+		System.out.println(x);
+		
+		if((false||(true||false))&&true&&false)
+		{
+			System.out.println("Success!!");
+		}
+		
+		
+	}
+
+}
