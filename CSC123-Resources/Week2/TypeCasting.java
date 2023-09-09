02665954275a6fb67bda1b5c526c7d0b44fe16eb
usman <usman@usmans-mbp>
2023-09-09 13:10:30 -0700

    first commit

---
+++ CSC123-Resources/Week2/TypeCasting.java
@@ -0,0 +1,22 @@
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		
+		//Downcasting
+		short s1=250;
+		byte b1=(byte)s1;	
+		System.out.println(b1);
+		
+		
+		//Casing 2
+		
+		int i=65;
+		char c=(char)i;
+		
+		System.out.println(c);
+
+		
+	}
+	
+}
