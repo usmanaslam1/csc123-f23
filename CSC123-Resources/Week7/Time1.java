42c3f667a5ecf8576159d0ebad619fd9007b6109
usman <usman@localhost>
2023-10-14 16:28:13 -0700

    Date arithmetic

--- CSC123-Resources/Week7/Time1.java
+++ CSC123-Resources/Week7/Time1.java
@@ -1,12 +1,48 @@
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class Time1 {
-	public static void main(String[] args) {
+	
+	static final long DATE_TO_YEARS_FACTOR=1000/60/60/24/365;
+	
+	public static void main(String[] args) throws Exception {
 		
 		
+		System.out.println(getAge("12/19/1943"));
 		
-		long currentTime=System.currentTimeMillis();
-		System.out.println(currentTime/1000/60/60/24/365);
 		
 		
 	}
+	
+	private static long getAge(String date) throws Exception{
+		SimpleDateFormat df=new SimpleDateFormat("MM/DD/YYYY");
+		Date dob=df.parse(date); //Date of birth
+ 		Date today=new Date(); //today's date
+		
+ 		//DOB milliseconds since 1970
+		long dobInMilliseconds=dob.getTime();
+		
+		//Today's date, milliseconds since 1970
+		long todayInMilliseconds=today.getTime();
+		
+		//Difference between today's date and DOB,  person's age in milliseconds 
+		long delta=todayInMilliseconds-dobInMilliseconds;
+		
+		//Person's age in years
+		long age=delta/1000/60/60/24/365;
+		
+		return age;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
