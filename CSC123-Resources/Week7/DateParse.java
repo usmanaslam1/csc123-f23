0ea45515ee927471db7c7d38e4a2621a34f700da
usman <usman@localhost>
2023-10-21 13:45:12 -0700

    commiting date parse

--- CSC123-Resources/Week7/DateParse.java
+++ CSC123-Resources/Week7/DateParse.java
@@ -3,17 +3,14 @@ import java.text.SimpleDateFormat;
 
 
 public class DateParse {
-	public static final String PATTERN="MMMM, dd, yyyy public static final String PATTERN=\"dd-mm-yyyy\"; HH:mm:ss";
+	public static final String PATTERN="dd-MM-yy";
 	public static void main(String[] args) {
 		
-		Date present=new Date();
 		SimpleDateFormat df=new SimpleDateFormat(PATTERN);
-		System.out.println(df.format(present));
 		
-		Date current=new Date(0);
-		System.out.println(df.format(current));
+		Date d2=df.parse("10-12-00");
+		System.out.println(d2);
 		
-
 	}
 
 }
